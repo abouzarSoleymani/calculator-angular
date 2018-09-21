b259a7ff1f47bdd6d927e6f73a20c11a7f8399bb
unknown <Malena Caroline>
2018-09-21 14:45:27 -0300

    testing calculator component

--- src/app/calculator/components/calculator.component.spec.ts
+++ src/app/calculator/components/calculator.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculatorComponent } from './calculator.component';
 import { CalculatorService } from '../services';
+import { By } from '../../../../node_modules/@angular/platform-browser';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -24,4 +25,30 @@ describe('CalculatorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css("#btn3"));
+    let btnSoma = fixture.debugElement.query(By.css("#btnSoma"));
+    let btn2 = fixture.debugElement.query(By.css("#btn2"));
+    let btnCalcular = fixture.debugElement.query(By.css("#btnCalcular"));
+    let display = fixture.debugElement.query(By.css("#display"));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    display.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  });
+
 });
